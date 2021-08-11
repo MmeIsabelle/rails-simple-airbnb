924e7284775ab397a6a855232c5ef75e7478abfe
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-11 13:29:47 -0700

    seeded 4 flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the database..."
+Flat.destroy_all
+
+puts "Adding flats..."
+4.times do
+  Flat.create!(
+    name: "#{Faker::Adjective} & #{Faker::Adjective} Flat in #{Faker::Address.city}",
+    address: Faker::Address.street_address,
+    description: "#{Faker::Marketing.buzzwords} - #{Faker::Lorem.sentence(word_count: 15)}",
+    price_per_night: rand(15..50) * 5,
+    number_of_guests: rand(1..6)
+  )
+end
+puts "4 flats created"
