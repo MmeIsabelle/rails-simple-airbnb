e312804f230bb7f5667efe23170fa111bdf3e20d
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-11 20:14:53 -0700

    built the view with simple cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Flat.destroy_all
 puts "Adding flats..."
 4.times do
   Flat.create!(
-    name: "#{Faker::Adjective} & #{Faker::Adjective} Flat in #{Faker::Address.city}",
+    name: "#{Faker::Adjective.positive.capitalize} & #{Faker::Adjective.positive.capitalize} Flat in #{Faker::Address.city}",
     address: Faker::Address.street_address,
     description: "#{Faker::Marketing.buzzwords} - #{Faker::Lorem.sentence(word_count: 15)}",
     price_per_night: rand(15..50) * 5,
